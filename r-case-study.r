cd3311b8550f15557010865993322d67ac485a74
Abdirahman Abdikadir Ali <36535646+abdimayow@users.noreply.github.com>
2025-01-27 11:37:47 +0300

    Kaggle Notebook | R Case Study | Version 1

---
+++ r-case-study.r
@@ -0,0 +1,116 @@
+{
+ "cells": [
+  {
+   "cell_type": "markdown",
+   "id": "a47c68fe",
+   "metadata": {
+    "_execution_state": "idle",
+    "_uuid": "051d70d956493feee0c6d64651c6a088724dca2a",
+    "execution": {
+     "iopub.execute_input": "2025-01-27T08:11:10.127615Z",
+     "iopub.status.busy": "2025-01-27T08:11:10.126008Z",
+     "iopub.status.idle": "2025-01-27T08:11:10.142009Z",
+     "shell.execute_reply": "2025-01-27T08:11:10.139172Z"
+    },
+    "papermill": {
+     "duration": 0.001908,
+     "end_time": "2025-01-27T08:37:39.309912",
+     "exception": false,
+     "start_time": "2025-01-27T08:37:39.308004",
+     "status": "completed"
+    },
+    "tags": []
+   },
+   "source": [
+    "<div style=\"text-align: center;\">\n",
+    "  <h1>Title: Cyclistic User Behavior Analysis</h1>\n",
+    "  <h2>Author: Abdirahman Abdikadir Ali</h2>\n",
+    "  <h4>Date: 25-01-2025</h4>\n",
+    "</div>\n",
+    "\n",
+    "# CONTEXT OF CASE STUDY\n",
+    "\n",
+    "### This is a case study project for Google Data Analytics Professional Certificate\n",
+    "\n",
+    "## About the Analysis\n",
+    "\n",
+    "<p>The analysis aims to explore how annual members and casual riders use Cyclistic bikes\n",
+    "differently, leveraging historical bike trip data to identify key usage patterns. \n",
+    "By understanding these differences, the study seeks to provide actionable insights to \n",
+    "support the business task of increasing the number of annual memberships, which are more \n",
+    "profitable for Cyclistic.</p>\n",
+    "\n",
+    "### Key Business Objectives\n",
+    "\n",
+    "1. **Understand User Behavior:** Analyze historical bike trip data to identify usage trends and behavior differences between casual riders and annual members.\n",
+    "2. **Support Marketing Strategies:** Provide insights into why casual riders might transition to annual memberships and how digital media could be used to influence this transition.\n",
+    "3. **Drive Growth:** Develop recommendations that focus on converting casual riders into annual members to maximize profitability and enhance the program`s long-term sustainability.\n",
+    "\n",
+    "## Deliverables\n",
+    "\n",
+    "The analysis includes:\n",
+    "\n",
+    "- A clear statement of the business task.\n",
+    "- A description of the data sources used.\n",
+    "- Documentation of data cleaning and manipulation steps.\n",
+    "- Summarized insights into user behavior differences.\n",
+    "- Supporting visualizations for key findings.\n",
+    "- Recommendations to guide future marketing campaigns.\n",
+    "\n",
+    "## About the Shiny App: Cyclistic_R\n",
+    "\n",
+    "The **Cyclistic_R** Shiny app is an interactive tool designed to showcase the results of this analysis. It fulfills the following objectives:\n",
+    "\n",
+    "1. **Visualization of Data Trends:** The app provides dynamic visualizations that highlight differences in ride duration, frequency, and station usage between casual riders and annual members.\n",
+    "2. **User Behavior Insights:** Users can explore specific patterns such as peak riding times, popular stations, and trip routes, helping to uncover insights about user preferences and habits.\n",
+    "3. **Data-Driven Recommendations:** The app supports decision-makers by presenting key findings in an accessible and engaging format, enabling informed marketing strategies to convert casual riders into members.\n",
+    "4. **Transparency and Interactivity:** It offers a platform for stakeholders to review the data cleaning, analysis process, and the methodology used in deriving recommendations.\n",
+    "\n",
+    "## Links\n",
+    "\n",
+    "- **[Shiny App](https://fy4z9s-mayow-cabdiqadir.shinyapps.io/case_study/)**\n",
+    "- **[GitHub Code Repository](https://github.com/abdimayow/Cyclistic_R)**\n",
+    "\n",
+    "\n"
+   ]
+  }
+ ],
+ "metadata": {
+  "kaggle": {
+   "accelerator": "none",
+   "dataSources": [],
+   "dockerImageVersionId": 30749,
+   "isGpuEnabled": false,
+   "isInternetEnabled": false,
+   "language": "r",
+   "sourceType": "notebook"
+  },
+  "kernelspec": {
+   "display_name": "R",
+   "language": "R",
+   "name": "ir"
+  },
+  "language_info": {
+   "codemirror_mode": "r",
+   "file_extension": ".r",
+   "mimetype": "text/x-r-source",
+   "name": "R",
+   "pygments_lexer": "r",
+   "version": "4.4.0"
+  },
+  "papermill": {
+   "default_parameters": {},
+   "duration": 3.369984,
+   "end_time": "2025-01-27T08:37:39.435877",
+   "environment_variables": {},
+   "exception": null,
+   "input_path": "__notebook__.ipynb",
+   "output_path": "__notebook__.ipynb",
+   "parameters": {},
+   "start_time": "2025-01-27T08:37:36.065893",
+   "version": "2.6.0"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
